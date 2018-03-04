da067c0892fc8727bfc29fb912eaa14a6251f3e4
Devin Matte <devinmatte@gmail.com>
2018-03-03 19:57:57 -0500

    Updating Config

--- config.env.py
+++ config.env.py
@@ -6,7 +6,7 @@ import string
 DEBUG = True
 IP = os.environ.get('SWAG_IP', '127.0.0.1')
 PORT = os.environ.get('SWAG_PORT', 8080)
-SERVER_NAME = os.environ.get('SWAG_SERVER_NAME', 'swag.csh.rit.edu')
+SERVER_NAME = os.environ.get('SWAG_SERVER_NAME', 'profiles.csh.rit.edu')
 
 # DB Info
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///{}'.format(os.path.join(os.getcwd(), "data.db")))
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", default=''.join(random.SystemRandom().choic
 # OpenID Connect SSO config
 OIDC_ISSUER = os.environ.get('SWAG_OIDC_ISSUER', 'https://sso.csh.rit.edu/auth/realms/csh')
 OIDC_CLIENT_CONFIG = {
-    'client_id': os.environ.get('SWAG_OIDC_CLIENT_ID', 'swag'),
+    'client_id': os.environ.get('SWAG_OIDC_CLIENT_ID', 'profiles'),
     'client_secret': os.environ.get('SWAG_OIDC_CLIENT_SECRET', ''),
-    'post_logout_redirect_uris': [os.environ.get('SWAG_OIDC_LOGOUT_REDIRECT_URI', 'https://swag.csh.rit.edu/logout')]
+    'post_logout_redirect_uris': [os.environ.get('SWAG_OIDC_LOGOUT_REDIRECT_URI', 'https://profiles.csh.rit.edu/logout')]
 }
